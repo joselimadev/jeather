c88a65c2db0ad9107debafd26848ff22747bdcd4
José Carlos <jcolscript@gmail.com>
2021-02-18 01:03:11 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'j-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {}
