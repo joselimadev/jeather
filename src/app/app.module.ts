197ae4820f4feb610e8ae4a787a271d9c03b3f81
José Carlos <jcolscript@gmail.com>
2021-02-23 02:05:18 -0300

    addind state effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +22,7 @@ import { environment } from '../environments/environment';
       maxAge: 25,
       logOnly: environment.production,
     }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
